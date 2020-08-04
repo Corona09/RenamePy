1e32006348721f0546bc184cb14558953cb6dd5f
Corona <Corona09@163.com>
2020-08-05 00:46:01 +0800

    add judge command conflict but not finished.

--- validity.py
+++ validity.py
@@ -2,18 +2,13 @@
 import error,processFuncs
 validCommand='ruUlLtTsSqzhbe'
 invalidNameChar='\\/:*?"<>|'
-def invalidCombi_of_SortOrder(orderLt):
-#---return orderAfterSep is not the valid combination of 'tTsSqzh'
-#---valid combination : t/T/s/S/q/z/h/*q/h*
-	if orderLt[0].isdigit():
-		return 406
-	if processFuncs.hasMoreThan_1_Elem_in(('t','T','s','S'),orderLt):
-		return 406
-	if 'z' in orderLt and len(orderLt)>1:
-		return 406
-	if 'h' in orderLt and len(orderLt)>2:
-		return 406
-	return 0
+
+def hasConflict_in(std_order):
+	conflictLt=[('u','U','l','L'),('t','T','s','S'),('e','b')]
+	for elem in conflictLt:
+		if processFuncs.hasMoreThan_1_Elem_in(elem,std_order):
+			return True
+	return False
 
 def notValid(raw):
 	rawAfterSplit=raw.split('\\')
@@ -25,3 +20,16 @@ def notValid(raw):
 	str1=rawAfterSplit[0]
 	str2=rawAfterSplit[1]
 	order='' if len(rawAfterSplit)==2 else rawAfterSplit[2]
+
+	if order=='':
+		if processFuncs.hasMem_in(invalidNameChar,str1) or processFuncs.hasMem_in(invalidNameChar,str2):
+			return 405
+		else:
+			return 0
+	else:
+		for elem in order:
+			if elem not in validCommand:
+				return 404
+		if processFuncs.hasMoreThan_1_NumSeq_in(order):
+			return 407
+		std_order=processFuncs.separate_num_and_letter(order)

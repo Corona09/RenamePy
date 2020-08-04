0ecf52a08a6d407ef4f85a40b817ab79e46b9086
Corona <Corona09@163.com>
2020-08-04 10:54:48 +0800

    add juege raw

--- validity.py
+++ validity.py
@@ -1,2 +1,12 @@
 #-*- coding:utf-8 -*-
 import error
+def judgeRaw(raw):
+	tmp=raw.split('\\')
+	if len(tmp)>3:
+		return 401
+	if len(tmp)<1:
+		return 402
+	return 0
+def judgeValid(str1,str2,command):
+	
+	return 0

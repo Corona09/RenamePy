bb53055d337aceec1cc9b9f0d5ec23c1d9a94cd9
Corona <Corona09@163.com>
2020-08-04 23:20:27 +0800

    add class singCommds & whether_empty

--- classCommands.py
+++ classCommands.py
@@ -1,7 +1,54 @@
 # -*- coding:utf-8 -*-
+class whether_empty:
+	def __init__(self,tup):
+		self.__whether_str1_empty=tup[0]
+		self.__whether_str2_empty=tup[1]
+	@property
+	def whether_str1_empty(self):return self.__whether_str1_empty
+	def whether_str2_empty(self):return self.__whether_str2_empty
+
+	def __add__(self,that):
+		ansTup=(1,1)
+		if self.whether_str1_empty
+class singleCommand:
+	def __init__(self,sinCommand):
+		self.__value=sinCommand
+		self.__whether_str12_empty=whether_empty({
+			#--- -1 : must be empty
+			#--- 0 : empty or not are both allowed
+			#--- 1 : cannot be empty
+			#--- up&lower command
+			'u':(1,0),
+			'l':(1,0),
+			'U':(0,0),
+			'L':(0,0),
+			#--- begin&end command
+			'e':(1,0),
+			'b':(1,0),
+			#---sort command
+			't':(-1,0),
+			'T':(-1,0),
+			's':(-1,0),
+			'S':(-1,0),
+			#---function command
+			'q':(0,0),
+			'z':(-1,-1),
+			'h':(-1,-1),
+			#---empty command:
+			'':(0,0)
+		}.get(sinCommand,(1,0)))
+	@property
+	def value(self):return self.__value
+	def whether_str1_empty(self):return self.__whether_str12_empty.whether_str1_empty
+	def whether_str2_empty(self):return self.__whether_str12_empty.whether_str2_empty
+	def whether_empty(self):return self.__whether_str12_empty
+
+	def __bool__(self):
+		return bool(self.value)
+
 class Command:
 	def __init__(self,number=None,reverse=None,upLower=None,beginEnd=None,Sort=None,Quit=None,cancel=None,Help=None):
-		func=lambda elem:elem if elem else ''
+		func=lambda elem:singleCommand(elem) if elem else singleCommand('')
 		self.__number=func(number)
 		self.__reverse=func(reverse)
 		self.__upLower=func(upLower)
@@ -29,6 +76,6 @@ class Command:
 	
 	def empty(self):
 		return not(self.number or self.reverse or self.upLower or self.beginEnd or self.sort or self.Quit or self.cancel or self.Help)
-	def __str__(self):
-		return self.number+self.reverse+self.upLower+self.beginEnd+self.sort+self.Quit+self.cancel+self.Help
-	
+
+	def whether_str_empty(self):
+		pass

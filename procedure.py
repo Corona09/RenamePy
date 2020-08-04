208934e6221a9d6fa4cf027e42b4a85cfcb8d930
Corona <Corona09@163.com>
2020-08-04 10:54:19 +0800

    add function splitRaw and mainProcess

--- procedure.py
+++ procedure.py
@@ -1,2 +1,10 @@
 #-*- coding:utf-8 -*-
 import error
+def splitRaw(raw):
+	raw=raw.split('\\')
+	str1=raw[0]
+	str2=raw[1]
+	command='' if len(raw)<3 else raw[2]
+	return tuple(str1,str2,command)
+def mainProcess(str1,str2,command):
+	return

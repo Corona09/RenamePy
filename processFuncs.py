000065e1200f6225868cd7654b77c719660d58a4
Corona <Corona09@163.com>
2020-08-04 12:14:44 +0800

    func:has more than 1 number seq in ()

--- processFuncs.py
+++ processFuncs.py
@@ -1,7 +1,17 @@
 #-*- coding:utf-8 -*-
 import error
-def hasMemNot_in(lt1,lt2):
-	for elem in lt1:
-		if elem not in lt2:
+def hasMoreThan1NumSeq_in(order):
+	number_beg=0
+	number_end=len(order)-1
+	for i in range(len(order)):
+		if order[i].isdigit():
+			number_beg=i
+			break
+	for j in range(len(order)):
+		if order[j].isdigit():
+			number_end=j
+			break
+	for k in range(number_beg,number_end+1):
+		if not order[k].isdigit():
 			return True
 	return False

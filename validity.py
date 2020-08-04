f4d6817bda1100fe3af405bcfcc8abcad6767562
Corona <Corona09@163.com>
2020-08-04 18:31:28 +0800

    add command order error 412

--- validity.py
+++ validity.py
@@ -26,17 +26,30 @@ def notValid(raw):
 	if processFuncs.hasMoreThan_1_NumSeq_in(order):
 		return 	407
 	
-	#---seperate the number and alpha in order and del the repeated order
+	#---seperate the number and letters in `order` and del the repeated order
+	#---orderAfterSep is a `list` 
+	#---if orderAfterSep has number order, it must be the first element.
 	orderAfterSep=processFuncs.separate_num_and_letter(order)
 
 	#---judge process begins:
 	if str1=='':
 		if str2=='':
 			if orderAfterSep=='':
-				return True #--error Code : ???
+				return 408 #--error Code : ???
 			else:
-				#reruen orderAfterSep is the valid combination of 'tTsSqzh'
-				pass
+				#return orderAfterSep is the valid combination of 'tTsSqzh'
+				# valid combination:
+				# t/T/s/S/q/z/h/*q/h*
+				if orderAfterSep[0].isdigit():
+					return 409
+				if processFuncs.hasMoreThan_1_Elem_in(('t','T','s','S'),orderAfterSep):
+					return 406
+				if 'z' in orderAfterSep and len(orderAfterSep)>1:
+					return 406
+				if 'h' in orderAfterSep and len(orderAfterSep)>2:
+					return 406
+				
+
 		else:
 			#if tTsS in orderAfterSep:
 			#	if str2 suits the order:

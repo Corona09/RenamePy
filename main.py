a34dadc4bbd855b53379a3d5586a0057ba85c140
Corona <Corona09@163.com>
2020-08-04 12:35:02 +0800

    new framework : separate the 'validity' and 'procedure' thoroughly

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 #-*- coding:utf-8 -*-
-import procedure, validity
+import error,procedure, validity
 count=1
 while True:
 	count+=1
 	raw=input() #--- to get the original operation order :str1\str2[\command]
-	if validity.judgeRaw(raw):
+	errCode=validity.notValid(raw)
+	if errCode:
+		error.warning(errCode)
 		continue
 	str1,str2,command=procedure.splitRaw(raw)
-	if validity.judgeValid(str1,str2,command):
-		continue
-	procedure.mainProcess(str1,str2,command)
-	if command.Quit=='q':
+	if procedure.mainProcess(str1,str2,command):#---func rturn True when need to do '\q'
 		break

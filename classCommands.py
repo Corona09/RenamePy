5a446d94ef91be6ccb670154fff1a7042ac32b65
Corona <Corona09@163.com>
2020-08-04 09:39:39 +0800

    add __str__() in class Command

--- classCommands.py
+++ classCommands.py
@@ -1,34 +1,32 @@
 # -*- coding:utf-8 -*-
 class Command:
-    def __init__(self,number=None,reverse=None,upLower=None,beginEnd=None,Sort=None,quit=None,cancel=None,help=None):
-        func=lambda elem:elem if elem else ''
-        self.__number=func(number)
-        self.__reverse=func(reverse)
-        self.__upLower=func(upLower)
-        self.__beginEnd=func(beginEnd)
-        self.__sort=func(Sort)
-        self.__quit=func(quit)
-        self.__cancel=func(cancel)
-        self.__help=func(help)
-    @property
-    def number(self):return self.__number
-    @property
-    def reverse(self):return self.__reverse
-    @property 
-    def upLower(self):return self.__upLower
-    @property
-    def beginEnd(self):return self.__beginEnd
-    @property
-    def sort(self):return self.__sort
-    @property
-    def quit(self):return self.__quit
-    @property
-    def cancel(self):return self.__cancel
-    @property
-    def help(self):return self.__help
-    
-    def __str__(self):
-        return ''
+	def __init__(self,number=None,reverse=None,upLower=None,beginEnd=None,Sort=None,Quit=None,cancel=None,Help=None):
+		func=lambda elem:elem if elem else ''
+		self.__number=func(number)
+		self.__reverse=func(reverse)
+		self.__upLower=func(upLower)
+		self.__beginEnd=func(beginEnd)
+		self.__sort=func(Sort)
+		self.__quit=func(Quit)
+		self.__cancel=func(cancel)
+		self.__help=func(Help)
+	@property
+	def number(self):return self.__number
+	@property
+	def reverse(self):return self.__reverse
+	@property 
+	def upLower(self):return self.__upLower
+	@property
+	def beginEnd(self):return self.__beginEnd
+	@property
+	def sort(self):return self.__sort
+	@property
+	def Quit(self):return self.__quit
+	@property
+	def cancel(self):return self.__cancel
+	@property
+	def Help(self):return self.__help
+	
+	def __str__(self):
+		return self.number+self.reverse+self.upLower+self.beginEnd+self.sort+self.Quit+self.cancel+self.Help
 
-a=Command()
-print(a)

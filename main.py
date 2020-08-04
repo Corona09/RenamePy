419102e10c6eca96622130c59b04eff70269ffff
Corona <Corona09@163.com>
2020-08-04 10:53:31 +0800

    change the judge process

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import procedure, validity
 count=1
 while True:
 	count+=1
-	str1,str2,command=procedure.getRawOperation() #--- to get the original operation order :str1\str2[\command]
-	if not validity.judgeValide(str1,str2,command):
+	raw=input() #--- to get the original operation order :str1\str2[\command]
+	if validity.judgeRaw(raw):
 		continue
-	procedure.do(str1,str2,command)
-	if command.quit():
+	str1,str2,command=procedure.splitRaw(raw)
+	if validity.judgeValid(str1,str2,command):
+		continue
+	procedure.mainProcess(str1,str2,command)
+	if command.Quit=='q':
 		break

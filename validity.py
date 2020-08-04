44ae419606f6f55a49224fab93a44c3243f3b409
Corona <Corona09@163.com>
2020-08-04 18:12:24 +0800

    main Judge Framwork

--- validity.py
+++ validity.py
@@ -28,5 +28,27 @@ def notValid(raw):
 	
 	#---seperate the number and alpha in order and del the repeated order
 	orderAfterSep=processFuncs.separate_num_and_letter(order)
-	
+
+	#---judge process begins:
+	if str1=='':
+		if str2=='':
+			if orderAfterSep=='':
+				return True #--error Code : ???
+			else:
+				#reruen orderAfterSep is the valid combination of 'tTsSqzh'
+				pass
+		else:
+			#if tTsS in orderAfterSep:
+			#	if str2 suits the order:
+			# 		return 0
+			# 	else:
+			# 		return True
+			#else:
+			#	return True # --- errCode : ??? 
+	else:
+		if '*' not in str2:
+			#---judge the orderAfterSep's validity with  
+			pass
+		else:
+			return True #---error Code : ???
 	return False

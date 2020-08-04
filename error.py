e3c616dafc318adfe85cdd16fd0e90663a5aa6ea
Corona <Corona09@163.com>
2020-08-04 10:31:38 +0800

    write error code and its information

--- error.py
+++ error.py
@@ -1,10 +1,25 @@
 #-*- coding -*-
 debugOn=True
-def warning(errorCode=None,information=None):
+def warning(errorCode=None):
     errorCode=errorCode if errorCode else 0
-    information=information if information else ''
+    information={
+		0:'All Correct.',
+		401:'Input Includes MORE THAN 2 "\\".',
+		402:'"\\" Missing.',
+		403:'Inupt Length out of Limit',
+		404:'Unknown Command.',
+		405:'Invalid Charactor in Filename.',
+		406:'Command Confilct.',
+		407:'Number Repeat.',
+		408:'Empty Input.',
+		409:'Nothing to Operate',
+		410:'Command Repeat.',
+		411:'Command Missing.',
+		501:'Unknown Error.'
+	}.get(errorCode)
     print('0x{err:0>6x}:{info}'.format(err=errorCode,info=information))
 def debug(information):
     if not debugOn:
         return None
     print(information)
+warning(0)
